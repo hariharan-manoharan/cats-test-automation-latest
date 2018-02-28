2bef2f393b158827ec3bf02fc9bf86e49c66a6d5
Saran <saran.kannan@fulcrum.net>
2018-02-28 10:08:36 +0530

    Revert "updated login page"
    This reverts commit f99c88c39910036ed4e71f361686104f7cc2dfd7.

--- src/main/java/businessComponents/WEBAPP/AIRTEL/LoginPage.java
+++ src/main/java/businessComponents/WEBAPP/AIRTEL/LoginPage.java
@@ -3,7 +3,6 @@ package main.java.businessComponents.WEBAPP.AIRTEL;
 import java.sql.Connection;
 import java.util.concurrent.locks.Lock;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import com.relevantcodes.extentreports.ExtentTest;
@@ -28,17 +27,6 @@ public class LoginPage extends Utility {
 			String environmentUrl = environmentVariables.get("EnvironmentURL");	
 			webdriver.get(environmentUrl);
 			test.log(LogStatus.PASS, "Application launched");
-			
-			webdriver.findElement(By.id("j_username")).sendKeys("catsadm");
-			webdriver.findElement(By.id("j_password")).sendKeys("catscats11");
-			webdriver.findElement(By.id("loginBtn")).click();
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
 	
 		}
 
